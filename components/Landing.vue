1598301dc1a36cc688c0cbda4882fa7e17a8ba86
Dwaipayan Ray <dwaipayanray1@gmail.com>
2020-06-29 02:47:09 +0530

    Image avatar

--- components/Landing.vue
+++ components/Landing.vue
@@ -1,19 +1,33 @@
 <template>
   <div class="hero is-light is-fullheight">
-    <div class="container py-10">
-      <div class="columns">
+    <div class="container py-25">
+      <div class="columns is-fullwidth">
         <div class="column is-full">
-          <figure class="image is-128x128">
-            <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">
-          </figure>
+          <div class="avatar">
+            <img src="~/static/images/dwaip.jpg">
+          </div>
         </div>
       </div>
     </div>
   </div>
+  </div>
 </template>
 
 <style scoped>
-.py-10{
-    padding-top: 10vh;
+.py-25{
+    padding-top: 25vh;
+}
+.avatar{
+    width: 12rem;
+    height: 100%;
+    position: relative;
+    margin: 0 auto;
+}
+img {
+    display: block;
+    border-radius: 50%;
+    position: absolute;
+    bottom: -42px;
+    border: 4px solid white;
 }
 </style>
